2be72dd37c5a8662e45ce218111b8ba968e54e44
Noam Sar-El <ncsarel@gmail.com>
2024-08-08 19:28:22 +0300

    Fix fonts for OnlyOffice

--- modules/onlyoffice/onlyoffice.nix
+++ modules/onlyoffice/onlyoffice.nix
@@ -1,19 +1,12 @@
 { config, pkgs, ... }:
 
-let
-  # Define specific Hebrew fonts from culmus
-  hebrewFonts = pkgs.culmus.overrideAttrs (old: rec {
-    fonts = [ "David" "FrankRuehl" "Miriam" "Narkisim" ];
-  });
-in
 {
-  environment.systemPackages = with pkgs; [
-    hebrewFonts
-  ];
-
-  fonts.fonts = [
-    hebrewFonts
-  ];
+  fonts = {
+    enableDefaultPackages = true;
+    packages = with pkgs; [
+      (pkgs.culmus.override { fonts = [ "David" "FrankRuehl" "Miriam" "Narkisim" ]; })
+    ];
+  };
 
   services.onlyoffice = {
     enable = true;
