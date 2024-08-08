3f2b7d46d8216267a29ec9c2ec4352baebcd8552
Noam Sar-El <ncsarel@gmail.com>
2024-08-08 19:16:13 +0300

    Install only specific culmus Hebrew fonts

--- modules/onlyoffice/onlyoffice.nix
+++ modules/onlyoffice/onlyoffice.nix
@@ -1,11 +1,18 @@
 { config, pkgs, ... }:
+
+let
+  # Define specific Hebrew fonts from culmus
+  hebrewFonts = pkgs.culmus.overrideAttrs (old: rec {
+    fonts = [ "David" "FrankRuehl" "Miriam" "Narkisim" ];
+  });
+in
 {
   environment.systemPackages = with pkgs; [
-    culmus
+    hebrewFonts
   ];
 
   fonts.fonts = [
-    pkgs.culmus
+    hebrewFonts
   ];
 
   services.onlyoffice = {
