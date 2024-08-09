152c41886e44a324735a5eb5809173f67cf824a9
Noam Sar-El <ncsarel@gmail.com>
2024-08-09 18:59:51 +0300

    Change theme to catppuccin-mocha

--- modules/stylix/stylix.nix
+++ modules/stylix/stylix.nix
@@ -1,7 +1,7 @@
-{pkgs, ... }:
+{config, pkgs, lib, ... }:
 
 {
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/dracula.yaml";
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
   stylix.image = /home/noams/Pictures/blobs-l.svg;
   stylix.polarity = "dark";
 }
