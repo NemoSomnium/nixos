e11bebf2c030963e08a8015327cdd47b47226d0e
Noam Sar-El <ncsarel@gmail.com>
2024-08-09 18:01:37 +0300

    Finalized setting up stylix with my current wallpaper and dracula theme(base16-schemes)

--- modules/stylix/stylix.nix
+++ modules/stylix/stylix.nix
@@ -1,5 +1,7 @@
 {pkgs, ... }:
 
 {
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/dracula.yaml"
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/dracula.yaml";
+  stylix.image = /home/noams/Pictures/blobs-l.svg;
+  stylix.polarity = "dark";
 }
